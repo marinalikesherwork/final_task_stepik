736aaa55cfc0117efbcc168b65c3d60a7d06c213
marinayashina <marina.yashina@t-systems.com>
2021-09-17 13:30:41 +0300

    move methods for login to BasePage, add testcases to login from product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+from .locators import BasePageLocators
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
@@ -10,6 +11,13 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+        
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
         
     def open(self):
         self.browser.get(self.url)
